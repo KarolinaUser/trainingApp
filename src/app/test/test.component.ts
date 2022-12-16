632cfdb9e780b3af4684a4ba4d2564bdf5283fc7
KarolinaUser <karolina.krakow@o2.pl>
2022-12-16 16:00:25 +0100

    Just list in test component

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DogHome } from '../mock-dog';
 
 @Component({
   selector: 'app-test',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+    //my new dog list 
+    dog = DogHome;
 
-  ngOnInit(): void {
-  }
+
+ngOnInit() {
+}
+
+
+
+  constructor() { }
 
 }
